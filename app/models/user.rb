74f35f5c471deb755c72eca1f5f6dfeb4af1d618
jhnegbrt <jhnegbrt@gmail.com>
2021-06-29 15:19:19 -0700

    adds ensure session token method

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,8 +5,17 @@ class User < ApplicationRecord
   validates :session_token, presence: true, uniqueness: true
   validates :password_digest, presence: true
 
+  def self.
+
   def password=(password)
     @password_digest = BCrypt::Password.create(password)
+  end
+
+  def reset_session_token
+  end
+
+  def ensure_session_token
+    session_token ||= SecureRandom.urlsafe_base64
   end
 
 end
